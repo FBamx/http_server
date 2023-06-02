01dc899876b1c48d5ddb70396e89bb7b6a5358eb
root <root@DESKTOP-0T01DAB>
2023-06-02 22:18:07 +0800

    add server && parse http request

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,9 +1,21 @@
 mod http_request;
 mod http_response;
 
-use crate::http_request::{HttpRequest, RequestLine};
+use crate::http_request::HttpRequest;
+use std::str;
+use std::{io::Read, net::TcpListener};
 
 fn main() {
-    let request = HttpRequest::from("GET /login HTTP/1.1\r\nContent-type:html\r\n\r\nnihao");
-    println!("{:#?}", request);
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Running on port 8080");
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        println!("Connection established!");
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+
+        let str = str::from_utf8(&buffer).unwrap();
+        let http_request = HttpRequest::from(str);
+        println!("{:#?}", http_request);
+    }
 }
