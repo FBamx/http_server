efc4398c52c5e4a29a7e0d536930621979b1c865
root <root@DESKTOP-0T01DAB>
2023-06-02 21:29:10 +0800

    change the name of http_request from http_request to HttpRequest && implement from trait for HttpRequest

--- http_server/src/http_request.rs
+++ http_server/src/http_request.rs
@@ -1,38 +1,114 @@
 use std::collections::HashMap;
 
-pub enum request_method {
+#[derive(Debug)]
+enum RequestMethod {
     GET,
     POST,
 }
 
-enum http_version {
+#[derive(Debug)]
+enum HttpVersion {
     V1,
     V2,
 }
 
-struct request_line {
-    method: request_method,
+#[derive(Debug)]
+pub struct RequestLine {
+    method: RequestMethod,
     resource: String,
-    version: http_version,
+    version: HttpVersion,
 }
 
-enum request_body {
-    Conteng(String),
+#[derive(Debug)]
+enum RequestBody {
+    Content(String),
     NONE,
 }
 
-pub struct http_request {
-    request_line: request_line,
+#[derive(Debug)]
+pub struct HttpRequest {
+    request_line: RequestLine,
     request_header: HashMap<String, String>,
-    request_body: request_body,
+    request_body: RequestBody,
 }
 
-impl From<&str> for request_method {
-    fn from(value: &str) -> Self {
-        if value == "GET" {
-            return request_method::GET;
+impl From<&str> for RequestMethod {
+    fn from(method: &str) -> Self {
+        if method == "GET" {
+            return RequestMethod::GET;
         } else {
-            return request_method::POST;
+            return RequestMethod::POST;
+        }
+    }
+}
+
+impl From<&str> for HttpVersion {
+    fn from(version: &str) -> Self {
+        if version == "HTTP/1.1" {
+            return HttpVersion::V1;
+        } else {
+            return HttpVersion::V2;
+        }
+    }
+}
+
+impl RequestLine {
+    fn new() -> Self {
+        return RequestLine {
+            method: RequestMethod::GET,
+            resource: String::from("/"),
+            version: HttpVersion::V1,
+        };
+    }
+}
+
+impl From<&str> for RequestLine {
+    fn from(request_line: &str) -> Self {
+        let mut word = request_line.split_whitespace();
+        let method = RequestMethod::from(word.next().unwrap());
+        let resource = word.next().unwrap();
+        let version = HttpVersion::from(word.next().unwrap());
+        return RequestLine {
+            method: method,
+            resource: resource.to_string(),
+            version: version,
+        };
+    }
+}
+
+fn process_header_line(s: &str) -> (String, String) {
+    let mut header_items = s.split(":");
+    let mut key = String::from("");
+    let mut value = String::from("");
+    if let Some(k) = header_items.next() {
+        key = k.to_string()
+    }
+    if let Some(v) = header_items.next() {
+        value = v.to_string()
+    }
+
+    (key, value)
+}
+
+impl From<&str> for HttpRequest {
+    fn from(http_request: &str) -> Self {
+        let mut request_line = RequestLine::new();
+        let mut request_header = HashMap::new();
+        let mut request_body = RequestBody::NONE;
+        for line in http_request.lines() {
+            if line.contains("HTTP") {
+                request_line = RequestLine::from(line);
+            } else if line.contains(":") {
+                let (key, value) = process_header_line(line);
+                request_header.insert(key, value);
+            } else {
+                request_body = RequestBody::Content(line.to_string());
+            }
+        }
+        HttpRequest {
+            request_line: request_line,
+            request_header: request_header,
+            request_body: request_body,
         }
     }
 }
--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,12 +1,9 @@
 mod http_request;
 mod http_response;
 
-use crate::http_request::request_method;
+use crate::http_request::{HttpRequest, RequestLine};
 
 fn main() {
-    let _method = request_method::from(String::from("GET").as_str());
-    match _method {
-        request_method::GET => println!("GET"),
-        request_method::POST => println!("POST"),
-    }
+    let request = HttpRequest::from("GET /login HTTP/1.1\r\nContent-type:html\r\n\r\nnihao");
+    println!("{:#?}", request);
 }
